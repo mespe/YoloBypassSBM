99a449cff2f3de16e1fd8816c8a8d2e7b073cd96
Travis Hinkelman <travis.hinkelman@gmail.com>
2020-05-01 12:10:25 -0700

    Add messages to reset_model_inputs

--- R/reset_model_inputs.R
+++ R/reset_model_inputs.R
@@ -10,25 +10,33 @@
 #'
 
 reset_model_inputs <- function(){
-  rm(list = c("annual_abundance",
-              "cohort_data_template",
-              "flood_duration",
-              "floodplain_temperature_difference",
-              "floodplain_temperature",
-              "freeport_flow",
-              "freeport_temperature",
-              "fremont_weir_proportion",
-              "growth_parameters",
-              "knights_landing_fl_params",
-              "knights_landing_timing",
-              "length_weight_parameters",
-              "ocean_survival_parameters",
-              "rearing_proportion_parameters",
-              "rearing_survival_parameters",
-              "rearing_time_parameters",
-              "simulation_parameters",
-              "telemetry_parameters",
-              "toe_drain_temperature"),
-     envir = .GlobalEnv)
+  model_objs <- c("annual_abundance",
+                  "cohort_data_template",
+                  "flood_duration",
+                  "floodplain_temperature_difference",
+                  "floodplain_temperature",
+                  "freeport_flow",
+                  "freeport_temperature",
+                  "fremont_weir_proportion",
+                  "growth_parameters",
+                  "knights_landing_fl_params",
+                  "knights_landing_timing",
+                  "length_weight_parameters",
+                  "ocean_survival_parameters",
+                  "rearing_proportion_parameters",
+                  "rearing_survival_parameters",
+                  "rearing_time_parameters",
+                  "simulation_parameters",
+                  "telemetry_parameters",
+                  "toe_drain_temperature")
+
+  model_objs_rm <- model_objs[model_objs %in% ls(envir = .GlobalEnv)]
+
+  if (length(model_objs_rm) > 0){
+    message("Removed the following YoloBypassSBM objects from the global environment: ", toString(model_objs_rm))
+    rm(list = model_objs_rm, envir = .GlobalEnv)
+  } else {
+    message("No YoloBypassSBM objects found in global environment.")
+  }
 }
 
