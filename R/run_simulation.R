e0cd8ef947907c57edbc3ab4a16ec10ab5bff615
Matt Espe <mespe@ucdavis.edu>
2020-05-14 08:52:06 -0600

    Parallelization for big speedup.

--- R/run_simulation.R
+++ R/run_simulation.R
@@ -10,7 +10,7 @@
 #'
 
 
-run_simulation <- function(){
+run_simulation <- function(cores = parallel::detectCores()){
 
   params <- simulation_parameters
   set.seed(params[["random_seed"]])
@@ -24,23 +24,23 @@ run_simulation <- function(){
     iters = expand.grid(params[["water_years"]],
                         params[["chinook_runs"]], stringsAsFactors = FALSE)
   # keeping Sac and Yolo separate because different number of columns
-  rep_list <- list()
-    for (i in params[["reps"]]){
+    rep_list = parallel::mclapply(params[["reps"]], function(i) {
+
         ## This part is done in a loop
         run_list = lapply(seq(nrow(iters)), function(i)
           run_one_rep(water_year = iters[i,1],
                       chinook_run = iters[i,2],
                       sim_type = params[["sim_type"]]))
-
+        names(run_list) = iters[,2]
         ## Summerize each water year
         wy_list = sapply(params[["water_years"]], function(wy){
             process_list(run_list[iters[,1] == wy], "Run")
         }, simplify = FALSE, USE.NAMES = TRUE)
                 
-        rep_list[[as.character(i)]] <- process_list(wy_list, "WaterYear")
+        process_list(wy_list, "WaterYear")
     # carriage return, \r, allows for rewriting on same line
-    cat("\r", "Rep", i, "of", max(params[["reps"]]))
-  }
+        #cat("\r", "Rep", i, "of", max(params[["reps"]]))
+    }, mc.cores = cores)
   cat("\n")  # expectation is that cursor will generally be placed on next line, but this is insurance
   if (!dir.exists("results")) dir.create("results")
 
