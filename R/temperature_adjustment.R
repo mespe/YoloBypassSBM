e4ffd4a91590384639747d622edd5a389e2f7572
Matt Espe <mespe@ucdavis.edu>
2020-05-14 08:29:10 -0600

    Small tweaks to temp adjustment fun.

--- R/temperature_adjustment.R
+++ R/temperature_adjustment.R
@@ -10,7 +10,7 @@
 #' @export
 #'
 #'
-
+if(FALSE){
 temperature_adjustment <- function(model_day, duration, location){
   # this function is not vectorized
 
@@ -31,5 +31,26 @@ temperature_adjustment <- function(model_day, duration, location){
   # mininum of exceed_indices is first day that temp was over threshold in the max potential rearing period
   exceed_indices <- which(temps_sub > thresh)
   return(ifelse(length(exceed_indices) > 0, min(exceed_indices), length(temps_all[model_day:model_day_end])))
+}
+}
+temperature_adjustment <- function(model_day, duration, location){
+    # this function is not vectorized
+    if (duration == 0) return(0)
+    
+    # Temperature adjustment
+    thresh <- rearing_time_parameters[[location]][["thresh"]]
+    temps_all <- floodplain_temperature[[location]][["Value"]]
+    
+    model_day_end <- min(model_day + duration, length(temps_all))
+    
+  # truncate rearing time to end of time period where input data available
+    temps_sub <- temps_all[model_day:model_day_end]
+    
+    # temps above threshold on every day
+    if (min(temps_sub) > thresh) return(0)
+
+    # mininum of exceed_indices is first day that temp was over threshold in the max potential rearing period
+    exceed_indices <- which(temps_sub > thresh)
+    return(if(length(exceed_indices)) exceed_indices[1] else model_day_end - model_day)
 }
 
